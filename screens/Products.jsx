1fb8968e8402e1b9754b2ae1c9f1b3bf21c991eb
Richarley12 <richar.arley12@gmail.com>
2023-05-02 18:59:29 -0500

    cambios a products

--- screens/Products.jsx
+++ screens/Products.jsx
@@ -1,39 +1,45 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,Switch } from 'react-native';
 import { TextInput, Button} from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons} from '@expo/vector-icons'
 import { useState,useEffect } from 'react';
 
 
 
-export default function ProductsScreen({navigation, route}) {
-    
-  return(
-    <View style={styles.container}>
-      <Text style={{marginBottom:10,fontFamily:"sans-serif", fontSize:20}}>Auto</Text>
-      <TextInput
-      style={{marginBottom:10}}
-      label="Número de placa"
-      mode="outlined"
-      right={<TextInput.Icon icon=""/>}>
-        </TextInput>
-      <TextInput
-      style={{marginBottom:10}}
-      label="Marca"
-      mode="outlined"
-      right={<TextInput.Icon icon=""/>} >
-      </TextInput>
-      <TextInput
-      style={{marginBottom:10}}
-      Estado
-      mode=""
-      right={<TextInput.Icon icon="eye"/>} 
-      >
-      </TextInput>
-    </View>
-  );
+export default function ProductsScreen({navigation, onPress}) {
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    return(
+        <View style={styles.container}>
+          <Text style={{marginBottom:10,fontFamily:"sans-serif", fontSize:20}}>Auto</Text>
+          <TextInput
+          style={{marginBottom:10}}
+          label="Número de placa"
+          mode="outlined"
+          right={<TextInput.Icon icon=""/>}>
+            </TextInput>
+          <TextInput
+          style={{marginBottom:10}}
+          label="Marca"
+          mode="outlined"
+          right={<TextInput.Icon icon=""/>} >
+          </TextInput>
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+          <Text>{isEnabled ? "Disponible" : "No disponible"}</Text>
+          <TouchableOpacity onPress={onPress}>
+          <View style={{ backgroundColor: 'blue', padding:10, marginTop:20 }}>
+            <Text style={{ color: 'white', textAlign: 'center', fontFamily:"Georgia" }}>Guardar</Text>
+          </View>
+        </TouchableOpacity>
+          
+        </View>
+      );
 }
 const styles = StyleSheet.create({
     container: {
